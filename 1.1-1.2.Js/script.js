8f83dc8e767f184d48dcb22afbfe8c56beefb691
Robpiot <robin.piot@outlook.com>
2023-09-08 14:23:04 +0200

    added first exercices 1.1 & 1.2, exercise are done

---
+++ 1.1-1.2.Js/script.js
@@ -0,0 +1,6 @@
+
+let firstName = "Gérald"
+let lastName = "Wilson"
+let age = 23
+let cityUser = "Mancherster"
+console.log ("Your full name is " + firstName + " " +lastName + " and you live in " + cityUser + ".")
