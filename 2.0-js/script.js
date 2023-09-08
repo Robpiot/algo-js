75055c7009206ec0fd9f6a2d5677176e7f82258f
Robpiot <robin.piot@outlook.com>
2023-09-08 16:58:49 +0200

    Done until 2.5

---
+++ 2.0-js/script.js
@@ -0,0 +1,79 @@
+// exo 2.1
+
+// let age = prompt();
+// if (parseInt(age) >= 18) {
+//     alert("you are an adult");
+// } else {
+//     alert("you are not an adult");
+// }
+
+
+// exo 2.2
+
+// let min = Number(prompt('Donner un minimum'));
+// let current = Number(prompt('Donner une valeur moyenne'));
+// let max = Number(prompt('Donner un maximum'));
+
+// if ( (min < current) && (current < max)) {
+//     alert(current +' is between ' + min + ' & ' + max)
+// } else if (min>max) {
+//     alert("Minimum is higher than maximum, that's not what was asked")
+// }
+// else  {
+//     alert (current + ' is not between ' + min + ' & ' + max)
+// }
+
+
+// EXO 2.3
+
+// let i = 1;
+// while(i<=100) {
+//     console.log(i);
+//     i ++;
+
+// }
+
+
+
+// for (let x = 1; x <= 50; x++) {
+//     console.log(x);
+    
+// }
+
+// EXO 2.4
+
+// for (let i = 1; i<=100; i++) {
+//  if (i%2 ==0) {
+//     console.log(i/2);
+// } else {
+//     console.log((i/2)*3);
+// }} 
+
+ // EXO 2.5
+ 
+ 
+//  let goodAnswer = 42;
+//  let userAnswer = Number(prompt('Enter the right number :'))
+
+//  do  {
+//      alert('Are you sure ? ')
+//      userAnswer = Number(prompt('Enter the right number : '));
+//  } while (userAnswer != goodAnswer) {
+//     alert('Welcome !')
+//  }
+    
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
