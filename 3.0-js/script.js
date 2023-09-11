b13b177acecb51b1cfbfb75229cde6073e98d45a
Robpiot <robin.piot@outlook.com>
2023-09-11 10:46:14 +0200

    Exercices 3 are done and all are working

---
+++ 3.0-js/script.js
@@ -0,0 +1,70 @@
+// exo 3.0
+
+// let classe = ["Alexandre Van", "Antoine", "Bastien", "Carole", "Dorian", "Elizabeth", "Elodie", "Justin", "Justine F", "Justine L", "Kimi", "Layla", "Lidwine", "Lucas", "Marie", "Mathias", "Okly", "Pierre M", "Robin", "Rosalie", "Stephane", "Tim", "Tom", "Valentin", "Alexandre Vens", "Virginie"];
+
+
+
+// for (let n of classe) {
+//     console.log("Here is the list of the Keller 6 : " + n);
+// }
+
+// EXO 3.1
+
+// it display all the items of the array
+
+// let nums = [1, 2, 3, 4 ,5]
+// let numbers = [100, 101, 102];
+// let sum = 0
+
+// for( let n of numbers) {
+//     sum =  sum+= n;
+//     console.log('The total of this array is :' + sum);
+// }
+
+// display only the sum answer
+
+// for (let n = 0; n< nums.length; n++){
+//     sum += nums[n]
+//     console.log('The total of this array is : ' + sum);
+// }
+
+// EXO 3.2
+
+// let nums = [1, 2, 3, 4 ,5]
+// let numbers = [100, 101, 102];
+// let sum = 0
+
+// for (let n of nums) {
+//     sum = sum+= n 
+//     let avg = sum/nums.length
+//     console.log('the average of the array is : ' + avg);
+// }
+
+//exo 3.3
+
+//  nums = [1, 2, 3, 4 ,5]
+// nums2 = []
+
+// for (i = 0; i< nums.length; i++) {
+//     nums2[i] = nums[i]
+
+    
+// } console.log('this is the original array ' + nums + " And this is the copy" + nums2);
+
+// push method 
+
+// nums = [1, 2, 3, 4, 5]
+//  nums2 = [];
+// nums2.push(...nums);
+// console.log(nums2);
+
+
+//exo 3.4
+
+
+// nums = [1, 2, 3, 4 ,5]
+
+// let minNums = Math.min(...nums)
+// let maxNums = Math.max(...nums)
+
+// console.log("The minimum is : " + minNums + " And the maximum is : " + maxNums);
