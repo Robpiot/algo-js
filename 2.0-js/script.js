4df2db3bdaaa47493869869adf90b36eb4f773a7
Robpiot <robin.piot@outlook.com>
2023-09-11 09:44:27 +0200

    End of the 2.0 exercices, every single exercise is working and done

--- 2.0-js/script.js
+++ 2.0-js/script.js
@@ -62,8 +62,54 @@
 //     alert('Welcome !')
 //  }
     
+
+
+// EXO 2.6
  
+// let day = Number(prompt("Pick a number bewtween 1 & 7 : "));
+
+// switch (day) {
+//     case 1 :
+//         alert('Monday')
+//         break;
+//     case 2 :
+//         alert('Tuesday')
+//         break;
+//     case 3: 
+//     alert('Wensday')
+//     break;
+//     case 4 :
+//         alert('thursday') 
+//         break;
+//     case 5 : 
+//         alert('Friday')
+//         break;
+//     case 6 :
+//         alert('Saturday')
+//         break;
+//     case 7 :
+//         alert('Sunday')
+//         break;
+//         default : 
+//         alert("You didn't pick a number between 1 & 7."+ "You entered :" + day);
+
+// }
+
+// EXO 2.7 
+
+let n = Number(prompt('Enter a number : '))
+let sum = 0
+
+for (i = 0; i<= n; i++){
+    sum =  sum + i;
+
+}
+
+    alert('Sum of ' + n +' is' + sum);
+    
+    
  
+
 
 
 
